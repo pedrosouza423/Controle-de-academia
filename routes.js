b60475693e9dfd24a03c460cc8476602cd58f464
pedrosouza423 <pedro.souza4237@gmail.com>
2021-01-14 20:23:09 -0300

    Foram feitas as rotas do site

--- routes.js
+++ routes.js
@@ -2,7 +2,15 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', function(req, res){
-    return res.send('Olá mundo')
+    return res.redirect("/instructors")
+})
+
+routes.get('/instructors', function(req, res){
+    return res.send("instructors")
+})
+
+routes.get('/members', function(req, res){
+    return res.send("members")
 })
 
 module.exports = routes
