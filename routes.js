a28cadef6e3f5ffb979505150ca8210b8c1ada09
pedrosouza423 <pedro.souza4237@gmail.com>
2021-01-15 00:09:04 -0300

    Foi feito o menu do layout

--- routes.js
+++ routes.js
@@ -6,7 +6,7 @@ routes.get('/', function(req, res){
 })
 
 routes.get('/instructors', function(req, res){
-    return res.send("instructors")
+    return res.render("instructors/index")
 })
 
 routes.get('/members', function(req, res){
