54056a55f451a9156f9cfcc4bd8121cf6adfa1a5
pedrosouza423 <pedro.souza4237@gmail.com>
2021-01-24 17:48:27 -0300

    Foi feito uma desestruturação de objeto e alterado a data de nascimento para milisegundos

--- instructors.js
+++ instructors.js
@@ -7,7 +7,7 @@ exports.post = function(req,res){
     // req.body
 
     const keys = Object.keys(req.body)
-
+    //validação
     for( key of keys ){
         //req.body.key
         if(req.body[key] == ''){
@@ -15,10 +15,25 @@ exports.post = function(req,res){
         }
     }
 
-    req.body.created_at = Date.now()
+    let {avatar_url, name, birth, gender, services} = req.body
+
+
+    //Tratamento nos dados
+    birth = Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number(data.instructors.length +1)
+
 
     // {...}
-    data.instructors.push(req.body)
+    data.instructors.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        gender,
+        services,
+        created_at
+    })
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
         if(err) return res.send("Write file error")
